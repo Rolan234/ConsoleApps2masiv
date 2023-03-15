3f85e524cd2f2107e1da774e61a43c1ca204d0a9
Rolan234 <124524243+Rolan234@users.noreply.github.com>
2023-03-15 18:33:56 +0200

    zadanie 2

--- ConsoleApp2masiv/ConsoleApp2masiv/Program.cs
+++ ConsoleApp2masiv/ConsoleApp2masiv/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,34 @@ namespace ConsoleApp2masiv
     {
         static void Main(string[] args)
         {
-            // Задача 1 
-            int[] array = new int[6];
-            for (int i = 0; i < 6; i++)  // последовательно
+            //// Задача 1 
+            //int[] array = new int[6];
+            //for (int i = 0; i < 6; i++)  // последовательно
+            //{
+            //    Console.WriteLine($"array [{i}] = ");
+            //    array[i] = int.Parse(Console.ReadLine() );
+            //}
+            //for (int i = array.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine($"array[{i}] = {array[i]}");
+            //}
+
+            // Задача 2
+
+            int[] array = new int[31];
+            Random random = new Random();
+            for (int i=0; i<array.Length; i++)
             {
-                Console.WriteLine($"array [{i}] = ");
-                array[i] = int.Parse(Console.ReadLine() );
+                array[i] = random.Next(5, 60);
+                Console.WriteLine($"array [{i}]= {array[i]}");
             }
-            for (int i = array.Length - 1; i >= 0; i--)
+            int summ = 0;
+            for (int i=0; i< array.Length; i++)
             {
-                Console.WriteLine($"array[{i}] = {array[i]}");
-        }
+                summ += array[i];
+            }
+            Console.WriteLine($"Общее кол-во осадков = {summ}");
             Console.Read();
+        }   
     }
 }
